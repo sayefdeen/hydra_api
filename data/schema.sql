014cdf325087f1c1fcb734de210712b453d2edf5
Sayefdeen <sayefdeen.alrawad@gmail.com>
2020-12-24 16:43:33 +0200

    data base tables are ready

--- data/schema.sql
+++ data/schema.sql
@@ -7,48 +7,97 @@ CREATE TYPE user_gender AS ENUM
 
 CREATE TABLE users
 (
-    u_id INT NOT NULL PRIMARY KEY,
+    u_id SERIAL NOT NULL PRIMARY KEY,
     -- primary key column
-    user_password [VARCHAR](255) NOT NULL,
-    oauth_token [VARCHAR](255) NOT NULL,
+    user_password VARCHAR (255) NOT NULL,
+    oauth_token VARCHAR (255) NOT NULL,
     user_role roles,
     is_activated boolean
 );
 
 CREATE TABLE buyers
 (
-    b_id INT NOT NULL PRIMARY KEY,
+    b_id SERIAL NOT NULL PRIMARY KEY,
     -- primary key column
-    u_id INT REFERENCES users (u_id) ON DELETE CASCADE ON  UPDATE CASCADE,
-    first_name [NVARCHAR](50) NOT NULL,
-    last_name [NVARCHAR](50) NOT NULL,
-    address [VARCHAR] (255),
+    u_id INTEGER REFERENCES users (u_id) ON DELETE CASCADE ON  UPDATE CASCADE,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    address VARCHAR  (255),
     gender user_gender
 );
 
 CREATE TABLE seller
 (
-    s_id INT NOT NULL PRIMARY KEY,
+    s_id SERIAL NOT NULL PRIMARY KEY,
     -- primary key column
-    u_id INT REFERENCES users (u_id) ON DELETE CASCADE ON  UPDATE CASCADE,
-    company_name [NVARCHAR](50) NOT NULL,
-    address [VARCHAR] (255),
-    telephone [NVARCHAR](50) NOT NULL,
+    u_id INTEGER REFERENCES users (u_id) ON DELETE CASCADE ON  UPDATE CASCADE,
+    company_name VARCHAR (50) NOT NULL,
+    address VARCHAR  (255),
+    telephone VARCHAR (50) NOT NULL
 );
 
 CREATE TABLE category
 (
-    id INT NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     -- primary key column
-    name [NVARCHAR](50) NOT NULL,
+    name VARCHAR (50) NOT NULL
 );
 
 CREATE TABLE products
 (
-    p_id INT NOT NULL PRIMARY KEY,
+    p_id SERIAL NOT NULL PRIMARY KEY,
     -- primary key column
-    s_id INT REFERENCES seller (s_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    name [NVARCHAR](150) NOT NULL,
-    description [TEXT],
-    main_img [TEXT],
+    s_id INTEGER REFERENCES seller (s_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    name VARCHAR (150) NOT NULL,
+    description TEXT,
+    main_img TEXT,
+    images TEXT
+    ARRAY,
+    price INTEGER,
+    c_id INTEGER REFERENCES category
+    (id) ON
+    DELETE CASCADE ON
+    UPDATE CASCADE,
+    quantity INTEGER,
+    is_deleted BOOLEAN,
+    is_bid BOOLEAN,
+    is_finsihed BOOLEAN,
+    start_time TIMESTAMP
+    DEFAULT CURRENT_TIMESTAMP,
+    end_time INTEGER
 );
+
+    CREATE TABLE seller_notify
+    (
+        id SERIAL NOT NULL PRIMARY KEY,
+        s_id INTEGER REFERENCES seller (s_id) ON UPDATE CASCADE on DELETE CASCADE,
+        p_id INTEGER REFERENCES products (p_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        b_id INTEGER REFERENCES buyers (b_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE buyer_fav
+    (
+        id SERIAL NOT NULL PRIMARY KEY,
+        b_id INTEGER REFERENCES buyers (b_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        p_id INTEGER REFERENCES products (p_id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+    CREATE TABLE buyer_cart
+    (
+        id SERIAL NOT NULL PRIMARY KEY,
+        b_id INTEGER REFERENCES buyers (b_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        p_id INTEGER REFERENCES products (p_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        quantity INTEGER,
+        is_bought BOOLEAN
+    );
+
+    CREATE TABLE buyer_comment
+    (
+        id SERIAL NOT NULL PRIMARY KEY,
+        b_c_id INTEGER REFERENCES buyer_cart (id) ON UPDATE CASCADE ON DELETE CASCADE,
+        comment TEXT,
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated TIMESTAMP,
+        is_deleted BOOLEAN
+    );
